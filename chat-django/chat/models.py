14a50866c79fc1b6b05f440b33507635b76e9055
viniciusmbpro <viniciusmbpro@gmail.com>
2023-04-14 17:58:49 -0300

    Adicionando related_name

--- chat-django/chat/models.py
+++ chat-django/chat/models.py
@@ -8,22 +8,33 @@ from core.base.models import BaseModel
 from django.db import models
 from accounts.models import Account
 
-class Chat(models.Model):
+
+class Chat(BaseModel):
     name = models.CharField(max_length=255)
-    participants = models.ManyToManyField(Account, related_name='chats', blank=True)
 
     def __str__(self):
         return self.name
 
+
+class ChatParticipant(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='chat_participants')
+
+    def __str__(self):
+        return f"{self.chat.name} - {self.user.email}"
+
+
 class Message(BaseModel):
     text = TextField(verbose_name="texto")
-    chat = ForeignKey(to="chat.Chat", verbose_name="para", related_name="messages_to", on_delete=PROTECT)
+    chat = ForeignKey(to="chat.Chat", verbose_name="para",
+                      related_name="messages_to", on_delete=PROTECT)
     terms = JSONField(verbose_name="termos", null=True, blank=True)
-    video_paths = JSONField(verbose_name="caminho de vídeos", null=True, blank=True)
+    video_paths = JSONField(
+        verbose_name="caminho de vídeos", null=True, blank=True)
 
     def __str__(self):
         return self.text
-    
+
     class Meta:
         verbose_name = "Mensagem"
         verbose_name_plural = "Mensagens"
