8b9e80c18571f3c757257c71e0393107f23ab800
viniciusmbpro <viniciusmbpro@gmail.com>
2023-06-08 13:16:33 -0300

    update requirements

--- chat-django/chat/urls.py
+++ chat-django/chat/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
     path(
         'accounts/dashboard/', 
         dashboard, 
-        name='dashboard'),
+        name='dashboard'
+    ),
     path(
         'chat/dashboard/new/',
         DashboardChat.as_view(),
@@ -43,54 +44,54 @@ urlpatterns = [
         'chat/<uuid:id>/',
         ChatDetail.as_view(), 
         name='chat-view'
-     ),
+    ),
     path(
         'chat/search/',
         ChatListViewSearch.as_view(), 
         name='search'
-     ),
+    ),
     path(
         'chat/add_message/<uuid:id>/',
         ChatAddMessageView.as_view(), 
         name='add-message'
-     ),
+    ),
 
     # api
     path(
         'api/chat/',
         ChatListCreateAPIView.as_view(), 
         name='api-list'
-     ),
+    ),
     path(
         'api/chat/<uuid:id>/',
         ChatDeleteUpdateAPIView.as_view(), 
         name='api-update'
-     ),
+    ),
     path(
         'api/chat/<uuid:id>/add_participant/',
         ChatAddParticipantAPIView.as_view(), 
         name='api-add-participant'
-     ),
+    ),
     path(
         'api/chat/<uuid:id>/add_message/',
         ChatAddMessageAPIView.as_view(), 
         name='api-add-message'
-     ),
+    ),
 
     # rotas de autenticação
     path(
         'api/token/',
         TokenObtainPairView.as_view(), 
         name='token_obtain_pair'
-     ),
+    ),
     path(
         'api/token/refresh/',
         TokenRefreshView.as_view(), 
         name='token_refresh'
-     ),
+    ),
     path(
         'api/token/verify/',
         TokenVerifyView.as_view(), 
         name='token_verify'
-     ),
+    ),
 ]
